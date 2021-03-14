02ff555d3ecbe9c83e9d24408651a460535563c9
nabeeltaariq <nabeeltaariq@gmail.com>
2021-03-14 04:17:30 -0700

    recalling concepts

--- First_C++/main.cpp
+++ First_C++/main.cpp
@@ -2,11 +2,27 @@
 
 using namespace std;
 
+class Alpha{
+private:
+    int a,b;
+public:
+    string initialize_it;
+    Alpha(){
+    a=b=0;
+    }
+    void okay(){
+    cout<<"Member of Alpha"<<endl;
+    cout<<"value of a: "<<a<<" and b: \t "<<b << "initialized \t " <<  initialize_it<<endl;
+    }
+};
+
+void void_practice();
 int main()
 {
-    int a=0;
-    string b="okay";
-    cout<<b;
-    cout << "I love CPP" << endl;
+  Alpha a;
+  a.initialize_it="Okay initialized";
+  a.okay();
+
     return 0;
 }
+
